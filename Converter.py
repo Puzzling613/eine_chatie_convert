7769f6530c55bc9fabb30e067dd326784d5dcbfc
Jominseok <jominseok@Jominseoks-MacBook-Pro.local>
2023-08-02 15:46:36 +0900

    지문에서 개행 추가, 모든 마크다운 오류 대응

--- Converter.py
+++ Converter.py
@@ -49,7 +49,7 @@ class ChatItem:
         rdict = {"id": self.id, "character_id": self.char_id, "object_type": "text", "object": self.iobject}
         return {key: value for key, value in rdict.items() if value is not None}
 
-
+"""
 class Image:
     def __init__(self, key, attachment_type, attachment):
         self.key = key
@@ -74,6 +74,7 @@ class Attachment:
         rdict = {"content_type": self.content_type, "url": self.url, "original_filename": self.original_filename,
                  "width": self.width, "hegiht": self.height, "checksum": self.checksum}
         return {key: value for key, value in rdict.items() if value is not None}
+"""
 
 
 class Object:
@@ -89,7 +90,7 @@ class Object:
     def option(self, options):
         return {"name": self.name, "options": options}
 
-
+"""
 class Options:
     def __init__(self, color, display_name_color, bubble_color, text_color):
         self.color = color
@@ -101,6 +102,14 @@ class Options:
         rdict = {"color": self.color, "display_name_color": self.display_name_color, "image": image,
                  "bubble_color": self.bubble_color, "text_color": self.text_color}
         return {key: value for key, value in rdict.items() if value is not None}
+"""
+
+
+def enter(sentence): # sentence는 string
+    sentence = sentence.replace(". ",".")
+    sentence = sentence.replace(".",".\n")
+    sentence = sentence.strip()
+    return sentence
 
 
 # prompt to chatie
@@ -140,27 +149,23 @@ def text2json(p):  # p는 str
                     teller_id = chara["id"]
 
         if teller == "전지적":
-            if line[0] == "*" and line[-2] != "*":
-                items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[1:len(line) - 1])).text())
-            elif line[0] != "*" and line[-2] == "*":
-                items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[0:len(line) - 2])).text())
-            elif line[0] == "*" and line[-2] == "*":
-                items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[1:len(line) - 2])).text())
-            else:
+            if line[0] == "*" and line[-1] != "*":
+                items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[1:len(line)])).text())
+            elif line[0] != "*" and line[-1] == "*":
                 items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[0:len(line) - 1])).text())
+            elif line[0] == "*" and line[-1] == "*":
+                items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[1:len(line) - 1])).text())
+            else:
+                items.append(ChatItem(str(uuid.uuid1()), "0", Object(None).text(line[0:len(line)])).text())
         else:
             if line[0:len(teller)] == teller:
                 if line[-1] == "." and line[-2] != ".": # 마침표
                     items.append(ChatItem(str(uuid.uuid1()), teller_id, Object(None).text(line[len(teller) + 1:len(line) - 1 ])).text())
-                #elif line[-1] == "\"":
-                    #items.append(ChatItem(str(uuid.uuid1()), teller_id, Object(None).text(line[len(teller) + 1:len(line) - 1 ])).text())
                 else:
                     items.append(ChatItem(str(uuid.uuid1()), teller_id, Object(None).text(line[len(teller) + 1:len(line)])).text())
             else:
                 if line[-1] == "." and line[-2] != ".": # 마침표
                     items.append(ChatItem(str(uuid.uuid1()), teller_id, Object(None).text(line[0:len(line) - 1])).text())
-                #elif line[-1] == "\"":
-                    #items.append(ChatItem(str(uuid.uuid1()), teller_id, Object(None).text(line[0:len(line) - 1])).text())
                 else:
                     items.append(ChatItem(str(uuid.uuid1()), teller_id, Object(None).text(line[0:len(line)])).text())
 
@@ -181,69 +186,68 @@ def json2text(messages): # message는 dictionary 가진 list
     action_finder = re.compile('^\*.*?\*')
     before_conv_finder = re.compile('^(.*?)\s*(?=")')
     before_action_finder = re.compile('^(.*?)\s*(?=\*)')
+    conv_action_order = re.compile('(\*.*?")|(".*?\*)')
     text = ""
-    ai_teller = messages[0]["name"]
-    user_teller = messages[1]["name"]
-
 
     for message in messages: # message는 dictionary
         enter_sentence = message["message"].split("\n")
         teller = message["name"]
-        # if teller not in characters:
-            # characters.append(teller)
+
         for sentence in enter_sentence:
             sentence = sentence.replace('\"*','\"').replace('*\"','\"')
             conv_or_action = 0 # 0이면 conv, 1이면 action
             while sentence != "":
-                if sentence != "" and sentence[0] == " ": sentence = sentence[1:] # 첫 공백 제거
-
-                if action_finder.match(sentence) is not None: # 지문
-                    text += action_finder.match(sentence).group()
-                    text += "\n"
+                sentence = sentence.strip()
+                # 정상적 마크다운
+                if action_finder.match(sentence) is not None: # *지문*
+                    text += "*"+ enter(action_finder.match(sentence).group()[1:-1]) + "*\n"
                     sentence = re.sub(action_finder,"",sentence)
                     conv_or_action = 0
                     continue
-                elif conversation_finder.match(sentence) is not None: # 대사
-                    text += teller + ": " + conversation_finder.match(sentence).group()[1:-1]
-                    text += "\n"
+                elif conversation_finder.match(sentence) is not None: # "대사"
+                    text += teller + ": " + conversation_finder.match(sentence).group()[1:-1] + "\n"
                     sentence = re.sub(conversation_finder,"",sentence)
                     conv_or_action = 1
                     continue
-                else: # 안 감싸진 것
-                    if teller == user_teller:
-                        if before_action_finder.match(sentence) is not None: # 뒤에 지문 있을 때
-                            text += user_teller+ ": " + before_action_finder.match(sentence).group() + "\n"
-                            sentence = re.sub(before_action_finder,"",sentence)
-                            conv_or_action = 1
-                            if before_action_finder.match(sentence).group() == "": #지문 처리(앞에만 *있을 경우)
-                                text += sentence + "\n"
-                                conv_or_action = 0
-                                break
-                            else: continue
-                        else:
-                            text += user_teller + ": " + sentence + "\n"
-                            conv_or_action = 1
+                else: # 안 감싸졌거나 부정확하게(하나만) 감싸진 것
+                    action_bf = before_action_finder.match(sentence)
+                    conv_bf= before_conv_finder.match(sentence)
+
+                    if action_bf is not None and conv_bf is None: # 뒤에 *까지 확인
+                        if action_bf.group() == "": # 처음에 *만 있는 경우 지문처리 (ex: *지문-)
+                            text += "*"+enter(sentence[1:]) + "\n"
                             break
-                    elif teller == ai_teller: # ai
-                        if before_conv_finder.match(sentence) is not None: #뒤에 대사 있을 때 지문 처리
-                            text += "*"+before_conv_finder.match(sentence).group()+"*" + "\n"
-                            sentence = re.sub(before_conv_finder,"",sentence)
-                            conv_or_action = 0
+                        else: # (ex: 대사 *지문*, 대사 *지문-) 
+                            text += teller+ ": " + action_bf.group() + "\n"
+                            sentence = re.sub(before_action_finder,"",sentence)
+                            # conv_or_action = 1
                             continue
-                        elif before_action_finder.match(sentence) is not None: #뒤에 지문 있을 때 대사 처리
-                            text += ai_teller + ": " + before_action_finder.match(sentence).group() + "\n"
+                    elif action_bf is not None and conv_bf is not None: # " * or * "
+                        if conv_action_order.search(sentence).group(1) is not None: # (ex: 지문* "대사", 지문* "대사-)
+                            text += "*" + enter(action_bf.group()[:-1]) + "\n"
+                            sentence = re.sub(before_conv_finder,"",sentence)
+                            # conv_or_action = 0
+                            continue
+                        elif conv_action_order.search(sentence).group(2) is not None: # (ex: 대사" *지문*, 대사" *지문-)
+                            text += teller + ": " + conv_bf.group() + "\n"
                             sentence = re.sub(before_action_finder,"",sentence)
-                            conv_or_action = 1
-                            if before_action_finder.match(sentence).group() == "": #지문 처리(앞에만 *있을 경우)
-                                text += sentence + "\n"
-                                conv_or_action = 0
-                                break # 이거 flow 확인 안 함 ===========================
-                            else: continue
-                        else: # 마지막 안 감싸짐
-                            if conv_or_action == 0:
+                            # conv_or_action = 1
+                            continue
+                    elif action_bf is None and conv_bf is not None: # 뒤에 "까지 확인
+                            if conv_bf.group() == "": # 처음에 "만 있는 경우 대사처리 (ex: "대사-)
                                 text += teller + ": " + sentence + "\n"
-                            else: text += "*"+ sentence + "*"+ "\n" #기본적으로 지문 처리인데 앞에 뭐 나왔는지에 따라서 달라짐 ===========================
-                            break
+                                break
+                            else: # (ex: 지문 "대사", 지문 "대사-) 
+                                text += "*"+ enter(conv_bf.group())+ "*" + "\n"
+                                sentence = re.sub(before_conv_finder,"",sentence)
+                                # conv_or_action = 0
+                            continue
+                    else: # 아무것도 안 감싸짐: action_bf is None and conv_bf is None (ex:대사 , 지문)
+                        if conv_or_action == 0:
+                                text += teller + ": " + sentence + "\n"
+                        else: 
+                            text += "*"+ enter(sentence) + "*"+ "\n"
+                        break
                     
     text += "="
     in_memory_file = io.StringIO()
